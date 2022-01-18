9025f94e664dafdd4c7ada24922aa19f6d766706
Kurdiamina3 <kurdiamina3@gmail.com>
2022-01-18 00:23:12 -0500

    redux installed

---
+++ client/build/precache-manifest.5edc8f5cfb22ec3a18166ad661e6cf68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0bb13d74c4d6b3a31d48c4ee9b8bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b75738aef1a09f6697",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "0541636a9569e8f813ab",
+    "url": "/static/js/2.5c678d53.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.5c678d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b75738aef1a09f6697",
+    "url": "/static/js/main.aa518c7b.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
